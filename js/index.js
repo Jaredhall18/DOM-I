5e7ea1931294e96170b60ec0fca8e3f342955464
jaredhall18 <jaredhall18@gmail.com>
2021-08-30 16:05:18 -0600

    Used querySelectors to Populate JS with header, main, and body sections

--- js/index.js
+++ js/index.js
@@ -39,4 +39,83 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+
+//Adding Nav Content
+// Linking each Anchor to a new variable using nextElement.
+const navText1 = document.querySelector('nav a');
+const navText2 = navText1.nextElementSibling;
+const navText3 = navText2.nextElementSibling;
+const navText4 = navText3.nextElementSibling;
+const navText5 = navText4.nextElementSibling;
+const navText6 = navText5.nextElementSibling;
+//Linking variables for each Anchor tag to Objects within siteContent Array
+navText1.textContent = siteContent['nav']['nav-item-1'];
+navText2.textContent = siteContent['nav']['nav-item-2'];
+navText3.textContent = siteContent['nav']['nav-item-3'];
+navText4.textContent = siteContent['nav']['nav-item-4'];
+navText5.textContent = siteContent['nav']['nav-item-5'];
+navText6.textContent = siteContent['nav']['nav-item-6'];
+
+
+// Call to Action Content Block
+const sectionOne = document.querySelector('.cta');
+const titleSectionOne = sectionOne.querySelector('h1');
+titleSectionOne.textContent = siteContent['cta']['h1'];
+//titleSectionOne.innerHTML = Dom<br> i
+const buttonSectionOne = sectionOne.querySelector('button');
+buttonSectionOne.textContent = siteContent['cta']['button'];
+
+//Call to Action Image
+const ctaImage = sectionOne.querySelector('img');
+ctaImage.setAttribute('src', siteContent['cta']['img-src']);
+
+//Main  Content Text Block
+//Features Text
+const sectionTwo = document.querySelector('.text-content');
+const featuresHead = sectionTwo.querySelector('h4');
+featuresHead.textContent = siteContent['main-content']['features-h4'];
+
+const featuresBody = sectionTwo.querySelector('p');
+featuresBody.textContent = siteContent['main-content']['features-content'];
+
+//Content Text
+const contentBlock2 = sectionTwo.nextElementSibling;
+const aboutHead = contentBlock2.querySelector('h4');
+aboutHead.textContent = siteContent['main-content']['about-h4'];
+
+const aboutBody = contentBlock2.querySelector('p');
+aboutBody.textContent = siteContent['main-content']['about-content'];
+
+//Middle Image
+const mainImage = document.querySelector('.middle-img');
+mainImage.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+//Services Text
+const services = document.querySelector('.bottom-content .text-content');
+const servicesHead = services.querySelector('h4');
+servicesHead.textContent = siteContent['main-content']['services-h4'];
+
+const servicesBody = services.querySelector('p');
+servicesBody.textContent = siteContent['main-content']['services-content'];
+//Products Text
+// const products = services.nextElementSibling;
+// const productHead = contentBlock2.querySelector('h4');
+// productHead.textContent = siteContent['main-content']['product-h4'];
+
+// const productBody = contentBlock2.querySelector('p');
+// productBody.textContent = siteContent['main-content']['product-content'];
+//Visions Text
+
+//Contact Section
+const contactSection = document.querySelector('.contact');
+const contactHeader = contactSection.querySelector('h4');
+contactHeader.textContent = siteContent['contact']['contact-h4'];
+
+const address = contactSection.querySelector('p');
+address.textContent = siteContent['contact']['address'];
+const phone = contactSection.querySelector('p');
+phone.textContent = siteContent['contact']['phone'];
+const email = contactSection.querySelector('p');
+email.textContent = siteContent['contact']['email'];
